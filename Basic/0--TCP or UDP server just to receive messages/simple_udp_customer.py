b40c31bff42b82f8208e0c82c0ef4ad100118294
tyfhxn <58685206+tyfhxn@users.noreply.github.com>
2023-07-08 23:16:21 +0800

    change

--- Basic/0--TCP or UDP server just to receive messages/simple_udp_customer.py
+++ Basic/0--TCP or UDP server just to receive messages/simple_udp_customer.py
@@ -1,12 +1,25 @@
 import socket
 
+import wmi
+
+
+def get_ethernet_ip():
+    c = wmi.WMI()
+    for interface in c.Win32_NetworkAdapterConfiguration(IPEnabled=True):
+        # print(interface.IPAddress[0] + '----' + interface.Description)
+        if 'Realtek PCIe GbE Family Controller' in interface.Description:
+            # print(interface)
+            ip = interface.IPAddress[0]
+            return ip
+    return None
+
+
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
     send_date = input("请输入数据：")
     if send_date == 'exit':
         break
-    hostname = socket.gethostname()
-    ip = socket.gethostbyname(hostname)
+    ip = get_ethernet_ip()
     udp_socket.sendto(send_date.encode('gbk'), (ip, 9999))
 
 udp_socket.close()
--- Basic/0--TCP or UDP server just to receive messages/simple_udp_server.py
+++ Basic/0--TCP or UDP server just to receive messages/simple_udp_server.py
@@ -8,9 +8,22 @@
 # UDP编程的客户端一般步骤是：
 import socket
 
+import wmi
+
+
+def get_ethernet_ip():
+    c = wmi.WMI()
+    for interface in c.Win32_NetworkAdapterConfiguration(IPEnabled=True):
+        # print(interface.IPAddress[0] + '----' + interface.Description)
+        if 'Realtek PCIe GbE Family Controller' in interface.Description:
+            # print(interface)
+            ip = interface.IPAddress[0]
+            return ip
+    return None
+
+
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-hostname = socket.gethostname()
-ip = socket.gethostbyname(hostname)
+ip = get_ethernet_ip()
 udp_socket.bind((ip, 9999))
 while True:
     receive_date = udp_socket.recvfrom(1024)
